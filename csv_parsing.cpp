9834dd9e332b9dc05f4ddb7f85eb194b9677edb0
scutta2000 <scuttari.pietro@gmail.com>
2019-08-25 22:22:04 +0200

    Inizio del parsing del csv. Per ora legge il fine e lo stampa.

---
+++ csv_parsing.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+  ifstream ip("bestiario_golarion.csv");
+
+  if(!ip.is_open()) std::cout << "ERROR: File Open" << '\n';
+
+  string clima;
+  string fonte;
+  string gs;
+  string nome;
+  string sottotipo;
+  string terreno;
+  string tipo;
+
+  while(ip.good()){
+
+    getline(ip,clima,',');
+    getline(ip,fonte,',');
+    getline(ip,gs,',');
+    getline(ip,nome,',');
+    getline(ip,sottotipo,',');
+    getline(ip,terreno,',');
+    getline(ip,tipo,'\n');
+
+    std::cout << "cilma: "<<clima << '\n';
+    std::cout << "fonte: "<<fonte << '\n';
+    std::cout << "gs: "<<gs << '\n';
+    std::cout << "sottotipo: "<<sottotipo << '\n';
+    std::cout << "terreno: "<<terreno << '\n';
+    std::cout << "tipo: "<<tipo << '\n';
+    std::cout << "-------------------" << '\n';
+  }
+
+  ip.close();
+}
